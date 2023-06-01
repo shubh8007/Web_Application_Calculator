4c477b041956203d29431541d81fc338b7c6e349
shubham suryawanshi <130492776+shubh8007@users.noreply.github.com>
2023-06-01 23:36:18 +0530

    Add files via upload

---
+++ cal.js
@@ -0,0 +1,24 @@
+
+
+
+exports.addition=function(n1,n2){
+    return n1+n2;
+
+}
+exports.substraction=function f(n1,n2){
+    return n1-n2;
+
+}
+
+exports.multiplication=function(n1,n2){
+    return n1*n2;
+}
+exports.division=function (n1,n2){
+return n1/n2;
+
+}
+exports.square=function(n){
+    return n*n;
+
+}
+
